29ef31bdafbcda48a5b0053ca5a08ca768f99edb
송창용 <thdckdyd123@naver.com>
2024-01-17 14:48:27 +0900

    실습 내용 추가

---
+++ 파이썬기초_0115/python_1/python_hw_1_4/hw_1_4.py
@@ -0,0 +1,15 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+
+str1 = '사과는 영어로'
+str2 = '바나나는 영어로'
+str3 = '키위는 영어로'
+
+fruit1 = 'apple'
+fruit2 = 'banana'
+fruit3 = 'kiwi'
+
+print(str1, fruit1)
+print(str2, fruit2)
+print(str3, fruit3)
