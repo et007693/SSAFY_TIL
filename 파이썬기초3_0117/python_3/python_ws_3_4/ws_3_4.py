7a6f2689b7757d91459f1619ab571008f0ff056b
Song <84127378+et007693@users.noreply.github.com>
2024-01-17 22:04:15 +0900

    Update ws_3_4.py

--- 파이썬기초3_0117/python_3/python_ws_3_4/ws_3_4.py
+++ 파이썬기초3_0117/python_3/python_ws_3_4/ws_3_4.py
@@ -15,4 +15,4 @@ name = ['김시습', '허균', '남영로', '임제', '박지원']
 age = [20, 16, 52, 36, 60]
 address = ['서울', '강릉', '조선', '나주', '한성부']
 
-print((list(map(create_user, name, age, address))))
+print(list(map(create_user, name, age, address)))
