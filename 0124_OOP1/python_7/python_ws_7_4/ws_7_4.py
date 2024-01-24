ca2aa5aaa99d4a7d57faefdddcc6535cfdfecd79
송창용 <thdckdyd123@naver.com>
2024-01-24 14:16:28 +0900

    0124 class method instance 추가

---
+++ 0124_OOP1/python_7/python_ws_7_4/ws_7_4.py
@@ -0,0 +1,14 @@
+# 아래 클래스를 수정하시오.
+class Shape:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def print_info(self):
+        print(f'Width: {self.width}')
+        print(f'Height: {self.height}')
+        print(f'Area: {self.height * self.width}')
+        print(f'Perimeter: {(self.height+self.width) * 2}')
+
+shape1 = Shape(5, 3)
+shape1.print_info()
