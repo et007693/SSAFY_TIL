9029ad0b7ef59775c9fec7e6663c43c0d2d2f0cc
unknown <thdckdyd123@naver.com>
2024-01-31 15:24:04 +0900

    2001 후기 추가

--- 0129~_알고리즘/0131_List2/swea_2001.py
+++ 0129~_알고리즘/0131_List2/swea_2001.py
@@ -1,4 +1,12 @@
 # swea_2001
+'''
+https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AY1S2R462h4DFAWX&contestProbId=AV5PzOCKAigDFAUq&probBoxId=AY1andpK_vQDFAWX&type=PROBLEM&problemBoxTitle=240131_List2_1&problemBoxCnt=3
+
+처음엔 델타값을 활용해서 진행하려고 했으나 파리채의 크기가 커질수록 비는 값이 생겼음
+-> 이중 for문을 사용하여 범위 개념으로 진행
+
+중간에 좌표가 자꾸 벗어나서 많이 헤멨음 -> 범위 정해주는 과정에서 문제가 있었는데 항상 어려운것 같다 
+'''
 T = int(input())
 
 for tc in range(T):
