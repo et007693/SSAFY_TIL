2ca8c78f817cda3ffc58a0df2bf93496aa870380
unknown <thdckdyd123@naver.com>
2024-01-31 15:17:59 +0900

    4837 후기 추가

--- 0129~_알고리즘/0131_List2/swea_4837.py
+++ 0129~_알고리즘/0131_List2/swea_4837.py
@@ -2,6 +2,20 @@
 '''
 https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AY1S2R462h4DFAWX&contestProbId=AXO-1Zh6aPEDFAXS&probBoxId=AY1andpK_vQDFAWX&type=USER&problemBoxTitle=240131_List2_1&problemBoxCnt=3
 
+비트 연산자
+& : 비트 단위로 and 연산
+| : 비트 단위로 or 연산
+<< : 피연산자의 비트 열을 왼쪽으로 이동
+>> : 피연산자의 비트 열을 오른쪽으로 이동
+
+n = len(arr) # 원소의 갯수
+for i in range(1<<n) # 1<<n : 부분 집합의 갯수(2**n)
+    for j in range(n): # 원소의 수만큼 비트를 비교
+        if i & (1 << j): # i의 j번 비트가 1인 경우
+            print(arr[j], end = ",") # j번 원소 출력
+
+어려웠던 점
+비트 연산자가 무엇인지, 왜 그런 결과가 나오는지
 '''
 T = int(input())
 A = [i for i in range(1, 13)]
