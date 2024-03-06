2952b186de4d202c281401bce8c4bb2d1e8f4364
송창용 <thdckdyd123@naver.com>
2024-03-06 18:58:58 +0900

    로봇청소기 수정

--- 백준/14503_bj(로봇 청소기).py
+++ 백준/14503_bj(로봇 청소기).py
@@ -1,9 +1,3 @@
-# 14503_bj(로봇 청소기)
-'''
-https://www.acmicpc.net/problem/14503
-'''
-import time
-
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
 
@@ -21,16 +15,13 @@ def check(start): # 4방향 확인
     return True # 4 방향이 모두 청소가 되어 있다면 True 반환
 
 def clean(start, d):
-    # time.sleep(0.3)
     global clean_cnt
-    print(start, d)
     x, y = start
     if room[y][x] == 0:
         room[y][x] = 1
         clean_cnt += 1
 
     result = check(start)
-    print(result)
 
     if result:
         # 뒤로 한칸, 방향 고정 -> (d+2)%4
@@ -39,26 +30,19 @@ def clean(start, d):
         else:
             return
     else:
-        d = ((d - 1) + 4) % 4
-        if room[y+dy[d]][x + dx[d]] == 0:
-            print('turn', d)
-            x += dx[d]
-            y += dy[d]
-            clean([x,y], d)
-
-        # while room[y+dy[((d-1)+4) % 4]][x+dx[((d-1)+4) % 4]] == 1:
-        #     print([x, y], d)
-            # print(d)
-        # x += dx[d]
-        # y += dy[d]
-        # clean([x, y], d)
+        while True:
+            d = ((d - 1) + 4) % 4
+            if room[y+dy[d]][x + dx[d]] == 0:
+                x += dx[d]
+                y += dy[d]
+                break
+        clean([x,y], d)
 
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 room = [list(map(int, input().split())) for _ in range(n)]
 clean_cnt = 0
 
-clean([r, c], d)
+clean([c, r], d)
 
 print(clean_cnt)
-print(room)
